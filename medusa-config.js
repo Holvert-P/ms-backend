19a9cc23ef67e8440bc7b97dcc66fae8fbbd527c
Holvert Josué Pérez Talavera <50637750+Holvert-P@users.noreply.github.com>
2022-08-17 21:23:02 -0600

    update module exports in medusa config

--- medusa-config.js
+++ medusa-config.js
@@ -63,14 +63,15 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
+    redis_url: REDIS_URL,
     database_url: DATABASE_URL,
     database_type: "postgres",
-    // database_database: "./medusa-db.sql",
-    // database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
   },
   plugins,
 };
