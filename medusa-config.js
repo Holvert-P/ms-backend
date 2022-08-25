da8e84f822a3ae504174c250496be66d26d826f5
Holvert Josué Pérez Talavera <50637750+Holvert-P@users.noreply.github.com>
2022-08-25 14:30:07 -0600

    update build script

--- medusa-config.js
+++ medusa-config.js
@@ -25,15 +25,14 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS ||
-  "http://localhost:7000,http://localhost:7001,https://mia-secret-admin.netlify.app";
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL || "postgres://localhost/book-store";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
@@ -64,15 +63,13 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    redis_url: REDIS_URL,
+    // redis_url: REDIS_URL,
     database_url: DATABASE_URL,
     database_type: "postgres",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
-    database_extra:
-      process.env.NODE_ENV !== "development"
-        ? { ssl: { rejectUnauthorized: false } }
-        : {},
+    // database_logging: true,
+    // database_extra: { ssl: { rejectUnauthorized: false } },
   },
   plugins,
 };
